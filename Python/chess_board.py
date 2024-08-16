45ccc8036a4040a17e686150de1f1e28c053db97
Heikki Peltomäki <54885089+pelthe@users.noreply.github.com>
2024-08-16 12:35:26 +0300

    Update chess_board.py

--- Python/chess_board.py
+++ Python/chess_board.py
@@ -28,5 +28,14 @@ board[0][1] = "KNIGHT"
 board[0][6] = "KNIGHT"
 board[7][1] = "KNIGHT"
 board[7][6] = "KNIGHT"
+board[0][2] = "BISHOP"
+board[0][5] = "BISHOP"
+board[7][2] = "BISHOP"
+board[7][5] = "BISHOP"
+board[0][3] = "QUEEN"
+board[7][3] = "QUEEN"
+board[0][4] = "KING"
+board[7][4] = "KING"
+
 
 print(board)
