5d10e98191f5a0f07fe55c8c95f3eec66ce1408e
Heikki Peltomäki <54885089+pelthe@users.noreply.github.com>
2024-08-19 10:30:59 +0300

    Create simple_def.py

---
+++ Python/simple_def.py
@@ -0,0 +1,7 @@
+def address(street, city, postal_code):
+    print("Your address is:", street, "St.,", city, postal_code)
+
+s = input("Street: ")
+p_c = input("Postal Code: ")
+c = input("City: ")
+address(s, c, p_c)
