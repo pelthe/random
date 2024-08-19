a30a71df7b71ba03f47afa0758965712411aa428
Heikki Peltomäki <54885089+pelthe@users.noreply.github.com>
2024-08-19 11:14:16 +0300

    Update day_calc.py

--- Python/day_calc.py
+++ Python/day_calc.py
@@ -1,12 +1,8 @@
 def is_year_leap(year):
-    if year % 4 != 0:
-        return False
-    elif year % 100 != 0:
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
         return True
-    elif year % 400 != 0:
-        return False
     else:
-        return True
+        return False
 
 def days_in_month(year, month):
     if year < 1582 or month < 1 or month > 12:
