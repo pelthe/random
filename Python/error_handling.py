469acbdbabb4a17161ae3c4da6dbb836df09fdf0
Heikki Peltomäki <54885089+pelthe@users.noreply.github.com>
2024-08-21 10:40:52 +0300

    Update error_handling.py

--- Python/error_handling.py
+++ Python/error_handling.py
@@ -9,3 +9,15 @@ while True:
         print("Sorry. I cannot divide by zero.")
     except:
         print("I don't know what to do...")
+
+''' Multiple errors in one branch:
+while True:
+    try:
+        number = int(input("Enter an int number: "))
+        print(5/number)
+        break
+    except (ValueError, ZeroDivisionError):
+        print("Wrong value or No division by zero rule broken.")
+    except:
+        print("Sorry, something went wrong...")
+'''
